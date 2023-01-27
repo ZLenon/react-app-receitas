3f7ed34094476b3115ef896601cf96a6eb01a1ef
LeandreXFC <leandro.sousa23tec@gmail.com>
2023-01-27 18:55:15 -0300

    Req 11 e 12 pronto

--- src/index.js
+++ src/index.js
@@ -4,20 +4,15 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import FetchFoodApi from './Context/FetchFoodApi';
-import FetchNameApi from './Context/FetchNameApi';
-import FirstLetter from './Context/FirstLetter';
+// import FetchNameApi from './Context/FetchNameApi';
+import FetchNameApi from './Context/FetchMeals';
 
 ReactDOM
   .createRoot(document.getElementById('root'))
   .render(
     <BrowserRouter>
       <FetchNameApi>
-        <FirstLetter>
-          <FetchFoodApi>
-            <App />
-          </FetchFoodApi>
-        </FirstLetter>
+        <App />
       </FetchNameApi>
     </BrowserRouter>,
   );
